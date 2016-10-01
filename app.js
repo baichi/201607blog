88766b3c702a646056a9b82f2b1d6efe3e150534
zhufengnodejs <zhufengnodejs@126.com>
2016-10-01 16:00:59 +0800

    渲染模板

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 //首页路由
 var index = require('./routes/index');
 //用户路由
@@ -6,6 +7,12 @@ var user = require('./routes/user');
 //文章路由
 var article = require('./routes/article');
 var app = express();
+//设置模板引擎
+app.set('view engine','html');
+//设置模板的存放根目录
+app.set('views',path.join(__dirname,'views'));
+//设置模板的渲染函数
+app.engine('html',require('ejs').__express);
 //如果请求的路径以下面这个路径开头的话
 app.use('/',index);
 app.use('/user',user);
