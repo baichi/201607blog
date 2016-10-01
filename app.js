b7ac29e5e4a59a09d802414ba28f539e8606b2c4
zhufengnodejs <zhufengnodejs@126.com>
2016-10-01 15:52:31 +0800

    实现了所有的路由

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
+//首页路由
+var index = require('./routes/index');
+//用户路由
 var user = require('./routes/user');
+//文章路由
+var article = require('./routes/article');
 var app = express();
 //如果请求的路径以下面这个路径开头的话
+app.use('/',index);
 app.use('/user',user);
-
-
-app.get('/article/add',function(req,res){
-    res.send('发表文章');
-});
+app.use('/article',article);
 app.listen(9090);
