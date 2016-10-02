6987ebb749eb7153c0cb668b267ddfb68d78e0d6
zhufengnodejs <zhufengnodejs@126.com>
2016-10-02 10:57:42 +0800

    session

--- lesson/2.session/server.js
+++ lesson/2.session/server.js
@@ -5,9 +5,19 @@ var app = express();
  * 当使用了session中间件之后，会在req.session的js对象
  * 就是这个客户端在服务器对应的数据对象
  */
-app.use(session());
-app.get('/visit',function(req,res){
+app.use(session({
+    secret:'zfpx'
+}));
 
-});
+app.get('/visit',function(req,res){
+    var count = req.session.count;
+    if(count){
+        count++;
+    }else{
+        count = 1;
+    }
+    req.session.count = count;
+    res.end(`欢迎第${count}次光临`)
+})
 
 app.listen(9090);
