14a500563803bc6e57d42899e5338cd123544229
zhufengnodejs <zhufengnodejs@126.com>
2016-10-02 13:01:13 +0800

    实现发表文章的功能

--- db/index.js
+++ db/index.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId;
 //连接数据库
 mongoose.connect('mongodb://localhost:27017/201607blog');
 //定义schema 规定了用户集合的文档的属性名和类型
+// 文档的_id ObjectId 主键
 var UserSchema = new mongoose.Schema({
     username:String,//用户名
     password:String,//密码
@@ -9,6 +11,15 @@ var UserSchema = new mongoose.Schema({
 });
 //定义Model
 exports.User = mongoose.model('User',UserSchema);
+
+var ArticleSchema = new mongoose.Schema({
+    title:String,//文章标题
+    content:String,//正文
+    user:{type:ObjectId,ref:'User'},//外人的主键-外键
+    createAt:Date//发表时间
+});
+exports.Article = mongoose.model('Article',ArticleSchema);
+
 
 
 
--- routes/article.js
+++ routes/article.js
@@ -3,4 +3,15 @@ var router = express.Router();
 router.get('/add', function (req, res) {
     res.render('article/add',{title:'发表文章'});
 });
+//保存文章
+/**
+ * 1. 得到表单的内容，请求体保存到了req.body
+ * 2. req.body.createAt = new Date()
+ * 3. req.body.user = 当前登录的用户的_id req.session.user._id
+ * 4. 取得文章的Model，create方法保存此对象
+ * 5. 如果保存失败，跳回发表文章页面继续填写，如果保存失败跳转到首页
+ */
+router.post('/add',function(req, res){
+
+});
 module.exports = router;
