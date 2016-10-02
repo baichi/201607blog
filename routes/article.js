59ba15fb03769517e40d78611eb09b20342e86c4
zhufengnodejs <zhufengnodejs@126.com>
2016-10-02 17:55:00 +0800

    独立数据库的配置

--- routes/article.js
+++ routes/article.js
@@ -2,7 +2,7 @@ var express = require('express');
 var Article = require('../db').Article;
 var router = express.Router();
 router.get('/add', function (req, res) {
-    res.render('article/add',{title:'发表文章'});
+    res.render('article/add',{title:'发表文章',article:{}});
 });
 //保存文章
 /**
@@ -14,17 +14,36 @@ router.get('/add', function (req, res) {
  */
 router.post('/add',function(req, res){
     var article = req.body;// title content
-    article.createAt = new Date();
-    //文章的user属性引用用户_id属性
-    article.user = req.session.user._id;
-    Article.create(article,function(err,doc){
-        if(err){
-            //返回上一个页面
-            res.redirect('back');
-        }else {
-            res.redirect('/');
-        }
-    });
+    var _id = article._id;
+    if(_id){//如果把ID传过来了意味着是修改
+        Article.update({_id},{
+            $set:{
+                title:article.title,
+                content:article.content
+            }
+        },function(err,result){
+            if(err){
+                res.redirect('back');
+            }else{
+                res.redirect('/article/detail/'+_id);
+            }
+        });
+    }else{
+        article.createAt = new Date();
+        //把原来的空的_id删除掉，以便mongoose帮你自动生成一个新的ID
+        delete article._id;
+        //文章的user属性引用用户_id属性
+        article.user = req.session.user._id;
+        Article.create(article,function(err,doc){
+            if(err){
+                console.error(err);
+                //返回上一个页面
+                res.redirect('back');
+            }else {
+                res.redirect('/');
+            }
+        });
+    }
 });
 /**
  * 1.把标题变成超链接
@@ -36,4 +55,21 @@ router.get('/detail/:_id',function(req,res){
       res.render('article/detail',{title:'文章详情',article:doc});
   })
 });
+//删除文章
+router.get('/delete/:_id',function(req,res){
+  Article.remove({_id:req.params._id},function(err,result){
+      if(err){
+          res.redirect('back');
+      }else{
+          res.redirect('/');
+      }
+  });
+});
+
+router.get('/update/:_id',function(req,res){
+   Article.findById(req.params._id,function(err,doc){
+       res.render('article/add',{title:'编辑文章',article:doc});
+   })
+});
+
 module.exports = router;
